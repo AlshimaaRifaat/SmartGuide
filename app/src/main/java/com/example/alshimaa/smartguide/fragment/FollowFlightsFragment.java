6e9ea9bf3411737aa067a66f93254f4733e6cb84
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-24 16:33:50 +0200

    initial design>>   follow flights+1

--- app/src/main/java/com/example/alshimaa/smartguide/fragment/FollowFlightsFragment.java
+++ app/src/main/java/com/example/alshimaa/smartguide/fragment/FollowFlightsFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.alshimaa.smartguide.R;
 
@@ -13,7 +14,7 @@ import com.example.alshimaa.smartguide.R;
  * A simple {@link Fragment} subclass.
  */
 public class FollowFlightsFragment extends Fragment {
-
+EditText searchEtext;
 
     public FollowFlightsFragment() {
         // Required empty public constructor
@@ -25,7 +26,20 @@ View view;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =inflater.inflate(R.layout.fragment_follow_flights, container, false);
+        init();
+
+        searchEtext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.main_frame_container,new DetailsFollowFlightsFragment())
+                        .commit();                    //remove it
+            }
+        });
         return view;
+    }
+
+    private void init() {
+        searchEtext=view.findViewById(R.id.follow_flights_edit_text_search);
     }
 
 }
