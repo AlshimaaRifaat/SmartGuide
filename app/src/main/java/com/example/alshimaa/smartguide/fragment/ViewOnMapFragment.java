42807e0085322f81f7b360c53bda12f6c8ab83b4
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-24 16:59:17 +0200

    initial design>>   follow flights+1

---
+++ app/src/main/java/com/example/alshimaa/smartguide/fragment/ViewOnMapFragment.java
@@ -0,0 +1,35 @@
+package com.example.alshimaa.smartguide.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alshimaa.smartguide.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ViewOnMapFragment extends Fragment {
+
+
+    public ViewOnMapFragment() {
+        // Required empty public constructor
+    }
+
+View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view= inflater.inflate(R.layout.fragment_view_on_map, container, false);
+        init();
+        return view;
+    }
+
+    private void init() {
+    }
+
+}
