9f6ce71df36def429f940d31dec063567f5b70a0
Alshimaa <alshimaa.rifaat@gmail.com>
2019-04-02 12:05:37 +0200

    +login api done

--- app/src/main/java/com/example/alshimaa/smartguide/SplashActivity.java
+++ app/src/main/java/com/example/alshimaa/smartguide/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.alshimaa.smartguide;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -8,9 +10,11 @@ import android.view.WindowManager;
 
 
 import com.example.alshimaa.smartguide.activity.MainActivity;
+import com.example.alshimaa.smartguide.activity.NavigationActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
+SharedPreferences sharedPreferences;
+public static String Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,9 @@ public class SplashActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        sharedPreferences=getSharedPreferences("default", Context.MODE_PRIVATE);
+        Login=sharedPreferences.getString("login_to_follow_flight",null);
 
         Thread timer=new Thread(  )
         {
@@ -31,9 +38,16 @@ public class SplashActivity extends AppCompatActivity {
                 {
                     e.printStackTrace();
                 }finally {
+                    if (Login!=null)
+                    {
+                        Intent intent=new Intent( SplashActivity.this,NavigationActivity.class);
+                        startActivity( intent );
+                    }else
+                    {
+                        Intent intent=new Intent( SplashActivity.this,MainActivity.class);
+                        startActivity( intent );
+                    }
 
-                    Intent intent=new Intent( SplashActivity.this,MainActivity.class);
-                    startActivity( intent );
                     finish();
                 }
             }
--- app/src/main/java/com/example/alshimaa/smartguide/fragment/LoginFragment.java
+++ app/src/main/java/com/example/alshimaa/smartguide/fragment/LoginFragment.java
@@ -2,22 +2,36 @@ package com.example.alshimaa.smartguide.fragment;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.alshimaa.smartguide.NetworkConnection;
 import com.example.alshimaa.smartguide.R;
+import com.example.alshimaa.smartguide.SplashActivity;
 import com.example.alshimaa.smartguide.activity.NavigationActivity;
+import com.example.alshimaa.smartguide.presenter.LoginPresenter;
+import com.example.alshimaa.smartguide.view.LoginView;
+import com.fourhcode.forhutils.FUtilsValidation;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
-    Button registerBtn;
+public class LoginFragment extends Fragment implements LoginView {
+    Button loginBtn;
+    EditText userEmail,userPassword;
+    LoginPresenter loginPresenter;
+
+    SharedPreferences.Editor sharedPref;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -30,23 +44,88 @@ View view;
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_login, container, false);
         init();
-        registerBtn.setOnClickListener(new View.OnClickListener() {
+        sharedPref=getContext().getSharedPreferences("default", Context.MODE_PRIVATE).edit();
+        Login();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                performRegister();
+                performLogin();
             }
         });
         return view;
     }
 
-    private void performRegister() {
-        Intent i = new Intent(getActivity(), NavigationActivity.class);
+    private void Login() {
+        loginPresenter=new LoginPresenter( getContext(),this );
+    }
+
+    private void performLogin() {
+        /*Intent i = new Intent(getActivity(), NavigationActivity.class);
         startActivity(i);
-        ((Activity) getActivity()).overridePendingTransition(0,0);
+        ((Activity) getActivity()).overridePendingTransition(0,0);*/
+        FUtilsValidation.isEmpty( userEmail,"من فضلك ادخل بريدك الالكترونى" );
+        FUtilsValidation.isEmpty( userPassword,"من فضك,ادخل كلمه المرور" );
+        validateEmail();
+        NetworkConnection networkConnection=new NetworkConnection( getContext() );
+        if (networkConnection.isNetworkAvailable( getContext() ))
+        {
+
+            if(!userEmail.getText().toString().equals( "" )&&
+                    !userPassword.getText().toString().equals("")&&
+                    validateEmail())
+            {
+                loginPresenter.getLoginResult( userEmail.getText().toString(),
+                        userPassword.getText().toString(),"ar" );
+            }
+
+            else
+            {
+                Toast.makeText( getContext(),"من فضلك, املأ البيانات ", Toast.LENGTH_SHORT ).show();
+            }
+
+        }else {
+            Toast.makeText( getContext(), "تأكد من اتصالك بالانترنت", Toast.LENGTH_SHORT ).show();
+        }
+    }
+    private Boolean validateEmail(){
+        String EMAIL=userEmail.getText().toString().trim();
+        if (EMAIL.isEmpty()||!isValidEmail(EMAIL)){
+            userEmail.setError("البريد الالكتروني خاطئ!");
+
+            return false;
+        }else if(!userEmail.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")){
+            userEmail.setError(("البريد الالكتروني خاطئ!"));
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidEmail(String Email)
+    {
+        return !TextUtils.isEmpty( Email )&&  android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches();
     }
 
     private void init() {
-        registerBtn=view.findViewById(R.id.login_btn_register);
+        loginBtn=view.findViewById(R.id.login_btn_register);
+        userEmail=view.findViewById(R.id.login_Etext_email);
+        userPassword=view.findViewById(R.id.login_Etext_password);
     }
 
+    @Override
+    public void showLoginResult(String UserToken) {
+        sharedPref.putString("login_to_follow_flight",UserToken);
+        sharedPref.apply();
+        SplashActivity.Login=UserToken;
+       // Toast.makeText(getContext(), UserToken, Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(getActivity(), NavigationActivity.class);
+        startActivity(i);
+        ((Activity) getActivity()).overridePendingTransition(0,0);
+        getActivity().finish();
+    }
+
+    @Override
+    public void showError() {
+
+    }
 }
