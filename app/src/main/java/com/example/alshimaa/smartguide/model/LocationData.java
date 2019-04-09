1190e90d46196b1a9058a953e2776f899201b4d2
Alshimaa <alshimaa.rifaat@gmail.com>
2019-04-09 11:30:38 +0200

    +navigat design+design +2 points on map +design

---
+++ app/src/main/java/com/example/alshimaa/smartguide/model/LocationData.java
@@ -0,0 +1,11 @@
+package com.example.alshimaa.smartguide.model;
+
+public class LocationData {
+    double latitude;
+    double longitude;
+
+    public LocationData(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
