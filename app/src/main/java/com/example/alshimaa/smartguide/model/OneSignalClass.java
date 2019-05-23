663e0f331e34a59e585d0990f9429e9f18d58e47
Alshimaa <alshimaa.rifaat@gmail.com>
2019-05-23 13:54:10 +0200

    start trip guide done+handle finish btn done+handle exception in view map driver & supervisor >>check add trip+icons in map+status in supervisor

---
+++ app/src/main/java/com/example/alshimaa/smartguide/model/OneSignalClass.java
@@ -0,0 +1,11 @@
+package com.example.alshimaa.smartguide.model;
+
+import android.app.Application;
+
+public class OneSignalClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: Add OneSignal initialization here
+    }
+}
